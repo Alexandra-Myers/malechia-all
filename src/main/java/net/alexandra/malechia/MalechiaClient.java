14718e463298a5fdb088b9ab4c4853ad38ad3005
Alexandra-Myers <bia416cat@gmail.com>
2022-09-03 19:40:18 -0400

    unported

--- src/main/java/net/alexandra/malechia/MalechiaClient.java
+++ src/main/java/net/alexandra/malechia/MalechiaClient.java
@@ -4,14 +4,15 @@ import net.alexandra.malechia.block.ModBlocks;
 import net.alexandra.malechia.block.QuiltOnlyBlocks;
 import net.alexandra.malechia.util.MalechiaModelPredicateProvider;
 import net.alexandra.malechia.util.TestKeybinds;
-import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.render.RenderLayer;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 import org.quiltmc.qsl.block.extensions.api.client.BlockRenderLayerMap;
 
 public class MalechiaClient implements ClientModInitializer {
 
 	@Override
-	public void onInitializeClient() {
+	public void onInitializeClient(ModContainer mod) {
 		BlockRenderLayerMap.put(RenderLayer.getTranslucent(), ModBlocks.INFINITY_STAINED_GLASS.get());
 		BlockRenderLayerMap.put(RenderLayer.getTranslucent(), ModBlocks.PURE_ETHEREAL_CRYSTAL_BLOCK.get());
 		BlockRenderLayerMap.put(RenderLayer.getCutout(), QuiltOnlyBlocks.ETHEREAL_BUSH.get());
--- src/main/java/net/alexandra/malechia/item/MalechiaBowItem.java
+++ src/main/java/net/alexandra/malechia/item/MalechiaBowItem.java
@@ -29,56 +29,70 @@ public class MalechiaBowItem extends RangedWeaponItem {
 
     @Override
     public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
-        if (user instanceof PlayerEntity) {
-            PlayerEntity playerEntity = (PlayerEntity)user;
-            ItemStack itemStack = playerEntity.getArrowType(stack);
-            boolean bl = (itemStack.getItem() instanceof ArrowItem && !itemStack.isEmpty());
-            if (bl) {
+        if (user instanceof PlayerEntity player) {
+            boolean bl = player.getAbilities().creativeMode || EnchantmentHelper.getLevel(Enchantments.INFINITY, stack) > 0;
+            ItemStack itemStack = player.getArrowType(stack);
+            if (!itemStack.isEmpty() || bl) {
                 if (itemStack.isEmpty()) {
                     itemStack = new ItemStack(Items.ARROW);
                 }
 
                 int i = this.getMaxUseTime(stack) - remainingUseTicks;
                 float f = getPullProgress(i);
-                if (!((double)f < 0.1D)) {
+                if (!((double)f < 0.1)) {
+                    float fatigue = this.getFatigueForTime(i);
                     boolean bl2 = bl && itemStack.isOf(Items.ARROW);
                     if (!world.isClient) {
                         ArrowItem arrowItem = (ArrowItem)(itemStack.getItem() instanceof ArrowItem ? itemStack.getItem() : Items.ARROW);
-                        PersistentProjectileEntity persistentProjectileEntity = arrowItem.createArrow(world, itemStack, playerEntity);
-                        persistentProjectileEntity.setVelocity(playerEntity.getPitch(), playerEntity.getYaw(), 0.0F, f * 10.0F, 0.0F);
-                        if (f == 1.0F) {
-                            persistentProjectileEntity.setCritical(true);
+                        PersistentProjectileEntity abstractArrow = arrowItem.createArrow(world, itemStack, player);
+                        abstractArrow.setProperties(player, player.getPitch(), player.getYaw(), 0.0F, f * 3.0F, 0.1F * fatigue);
+                        if (f == 1.0F && fatigue <= 0.5F) {
+                            abstractArrow.setCritical(true);
                         }
 
                         int j = EnchantmentHelper.getLevel(Enchantments.POWER, stack);
                         if (j > 0) {
-                            persistentProjectileEntity.setDamage(persistentProjectileEntity.getDamage() + (double)j * 0.375D + 0.5D);
+                            abstractArrow.setDamage(abstractArrow.getDamage() + (double)j * 0.5 + 0.5);
                         }
 
                         int k = EnchantmentHelper.getLevel(Enchantments.PUNCH, stack);
                         if (k > 0) {
-                            persistentProjectileEntity.setPunch(k);
+                            abstractArrow.setPunch(k);
                         }
 
                         if (EnchantmentHelper.getLevel(Enchantments.FLAME, stack) > 0) {
-                            persistentProjectileEntity.setOnFireFor(100);
+                            abstractArrow.setOnFireFor(100);
                         }
 
-                        stack.damage(1, playerEntity, (p) -> {
-                            p.sendToolBreakStatus(playerEntity.getActiveHand());
+                        stack.damage(1, player, (p) -> {
+                            p.sendToolBreakStatus(player.getActiveHand());
                         });
-                        if (bl2 || playerEntity.getAbilities().creativeMode && (itemStack.isOf(Items.SPECTRAL_ARROW) || itemStack.isOf(Items.TIPPED_ARROW))) {
-                            persistentProjectileEntity.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
+                        if (bl2 || player.getAbilities().creativeMode && (itemStack.isOf(Items.SPECTRAL_ARROW) || itemStack.isOf(Items.TIPPED_ARROW))) {
+                            abstractArrow.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
                         }
 
-                        world.spawnEntity(persistentProjectileEntity);
+                        world.spawnEntity(abstractArrow);
                     }
 
-                    world.playSound((PlayerEntity)null, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(), SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.PLAYERS, 1.0F, 1.0F / (world.getRandom().nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+                    world.playSound((PlayerEntity)null, player.getX(), player.getY(), player.getZ(), SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.PLAYERS, 1.0F, 1.0F / (world.getRandom().nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+                    if (!bl2 && !player.getAbilities().creativeMode) {
+                        itemStack.decrement(1);
+                        if (itemStack.isEmpty()) {
+                            player.getInventory().removeOne(itemStack);
+                        }
+                    }
 
-                    playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+                    player.incrementStat(Stats.USED.getOrCreateStat(this));
                 }
             }
+        }
+    }
+
+    public float getFatigueForTime(int f) {
+        if (f < 60) {
+            return 0.5F;
+        } else {
+            return f >= 200 ? 10.5F : 0.5F + 10.0F * (float)(f - 60) / 140.0F;
         }
     }
 
